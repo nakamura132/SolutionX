e9932a26162e591f76e2fc8d3f2e6b8bd0adffe8
nakamura132 <windpublic@gmail.com>
2024-07-31 23:22:52 +0900

    .NET8.0から導入されたTimeProviderのトライアルプロジェクトを作成

---
+++ TimeProviderTry1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TimeProviderTry1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //var timeProvider = TimeProvider.System;
+            //var now = timeProvider.GetUtcNow();
+            //Console.WriteLine( $"現在の時刻: {now}" );
+
+            var customTime = new DateTimeOffset(2023, 7, 31, 12, 0, 0, TimeSpan.Zero);
+            var timeProvider = new CustomDateTimeProvider(customTime);
+
+            var now = timeProvider.GetUtcNow();
+            Console.WriteLine($"カスタム時刻: {now}");
+        }
+    }
+
+    // カスタム DateTimeProvider の作成と使用
+    class CustomDateTimeProvider : TimeProvider
+    {
+        private readonly DateTimeOffset _customTime;
+        public CustomDateTimeProvider(DateTimeOffset customTime)
+        {
+            _customTime = customTime;
+        }
+
+        public override DateTimeOffset GetUtcNow() => _customTime;
+    }
+}
